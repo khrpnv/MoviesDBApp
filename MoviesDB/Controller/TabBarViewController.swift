34cb6c967f0dfa8ce5564876c4abded29d603ebd
Illia Khrypunov <illia.khrypunov@nure.ua>
2019-12-01 23:50:46 +0200

    Favourites screen implementation

--- MoviesDB/Controller/TabBarViewController.swift
+++ MoviesDB/Controller/TabBarViewController.swift
@@ -20,6 +20,11 @@ class TabBarViewController: UITabBarController {
     moviesVC.tabBarItem = UITabBarItem(title: "Movies", image: #imageLiteral(resourceName: "listIcon"), tag: 0)
     moviesVC.modalPresentationStyle = .fullScreen
     
+    let favouritesVC = UINavigationController(rootViewController: FavouritesViewController(nibName: "FavouritesViewController", bundle: nil))
+    favouritesVC.title = "Favourites"
+    favouritesVC.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
+    favouritesVC.modalPresentationStyle = .fullScreen
+    
     self.tabBar.barStyle = .black
     self.tabBar.isTranslucent = false
     self.tabBar.tintColor = #colorLiteral(red: 0.9843137255, green: 0.5490196078, blue: 0, alpha: 1)
